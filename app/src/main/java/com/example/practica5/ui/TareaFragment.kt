13956c12f7bb1613d16c9e828bdf65f3209e61db
Pacoalberola <falberola2003@gmail.com>
2023-01-10 13:54:42 +0100

    Commit punto40

--- app/src/main/java/com/example/practica5/ui/TareaFragment.kt
+++ app/src/main/java/com/example/practica5/ui/TareaFragment.kt
@@ -1,13 +1,17 @@
 package com.example.practica5.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import com.example.practica5.R
 import com.example.practica5.databinding.FragmentTareaBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -36,7 +40,69 @@ class TareaFragment : Fragment() {
 //        binding.buttonSecond.setOnClickListener {
 //            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
 //        }
+        iniciaSpCategoria()
+        iniciaSpPrioridad()
+
+
     }
+
+    private fun iniciaSpCategoria() {
+        ArrayAdapter.createFromResource(
+            //contexto suele ser la Activity
+            requireContext(),
+            //array de strings
+            R.array.categoria,
+            //layout para mostrar el elemento seleccionado
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Layout para mostrar la apariencia de la lista
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // asignamos el adaptador al spinner
+            binding.spCategoria.adapter = adapter
+        }
+
+        //recuperamos el valor
+        val valor=binding.spCategoria.getItemAtPosition(2)
+        //creamos el mensaje desde el recurso string parametrizado
+        val mensaje=getString(R.string.mensaje_categoria,valor)
+        //mostramos el mensaje donde "binding.root" es el ContrainLayout principal
+        Snackbar.make(binding.root, mensaje, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+
+
+    }
+
+    private fun iniciaSpPrioridad(){
+        ArrayAdapter.createFromResource(
+            //contexto suele ser la Activity
+            requireContext(),
+            //array de strings
+            R.array.prioridad,
+            //layout para mostrar el elemento seleccionado
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Layout para mostrar la apariencia de la lista
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // asignamos el adaptador al spinner
+            binding.spPrioridad.adapter = adapter
+            binding.spPrioridad.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(p0: AdapterView<*>?, v: View?, posicion: Int, id: Long) {
+                    //el array son 3 elementos y "alta" ocupa la tercera posición
+                    if(posicion==2){  binding.clytTarea.setBackgroundColor(requireContext().getColor(R.color.prioridad_alta))
+                    }else{//si no es prioridad alta quitamos el color
+                        binding.clytTarea.setBackgroundColor(Color.TRANSPARENT)
+                    }
+
+                }
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                    binding.clytTarea.setBackgroundColor(Color.TRANSPARENT)
+                }
+            }
+        }
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
