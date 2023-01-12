570ac330dd58a5a2a59e11e3d6482a832d7a2ec7
Pacoalberola <falberola2003@gmail.com>
2023-01-12 13:21:51 +0100

    Commit punto60

--- app/src/main/java/com/example/practica5/ui/TareaFragment.kt
+++ app/src/main/java/com/example/practica5/ui/TareaFragment.kt
@@ -43,6 +43,7 @@ class TareaFragment : Fragment() {
         iniciaSpCategoria()
         iniciaSpPrioridad()
         iniciaSwPagado()
+        iniciaRgEstado()
 
 
     }
@@ -121,5 +122,20 @@ class TareaFragment : Fragment() {
         //iniciamos a valor false
         binding.swPagado.isChecked=false
         binding.ivPagado.setImageResource(R.drawable.ic_no_pagado)
+    }
+
+    private fun iniciaRgEstado() {
+        //listener de radioGroup
+        binding.rgEstado.setOnCheckedChangeListener { _, checkedId ->
+            val imagen= when (checkedId){//el id del RadioButton seleccionado
+                //id del cada RadioButon
+                R.id.rbAbierta-> R.drawable.ic_abierto
+                R.id.rbEnCurso->R.drawable.ic_encurso
+                else-> R.drawable.ic_cerrado
+            }
+            binding.ivEstado.setImageResource(imagen)
+        }
+        //iniciamos a abierto
+        binding.rgEstado.check(R.id.rbAbierta)
     }
 }
