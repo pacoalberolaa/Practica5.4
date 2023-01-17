fa23492781a1c5609d9a6941fe0f17c8f59dd9ea
Pacoalberola <falberola2003@gmail.com>
2023-01-17 19:14:25 +0100

    Commit inicio punto27

--- app/src/main/java/com/example/practica5/ui/ListaFragment.kt
+++ app/src/main/java/com/example/practica5/ui/ListaFragment.kt
@@ -1,5 +1,7 @@
 package com.example.practica5.ui
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -129,9 +131,31 @@ class ListaFragment : Fragment() {
             //***********Borrar Tarea************
             override fun onTareaBorrarClick(tarea: Tarea?) {
                 //borramos directamente la tarea
-                viewModel.delTarea(tarea!!)
+                //viewModel.delTarea(tarea!!)
+                if (tarea != null) {
+                    borrarTarea(tarea)
+                }
             }
 
         }
+    }
+
+    fun borrarTarea(tarea:Tarea){
+        AlertDialog.Builder(activity as Context)
+            .setTitle(android.R.string.dialog_alert_title)
+            //recuerda: todo el texto en string.xml
+            .setMessage("Desea borrar la Tarea ${tarea.id}?")
+            //acción si pulsa si
+            .setPositiveButton(android.R.string.ok){v,_->
+                //borramos la tarea
+                viewModel.delTarea(tarea)
+                //cerramos el dialogo
+                v.dismiss()
+            }
+            //accion si pulsa no
+            .setNegativeButton(android.R.string.cancel){v,_->v.dismiss()}
+            .setCancelable(false)
+            .create()
+            .show()
     }
 }
